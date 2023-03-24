418feab702c3d12eba475ecf13a1e67c791db1c8
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2023-03-24 22:56:35 +0100

    update practice

--- 3_Day/Repeat/test,py.py
+++ 3_Day/Repeat/test,py.py
@@ -1,14 +1,39 @@
-import matplotlib.pyplot as plt
+import os
+from tkinter import filedialog, Tk
+from PIL import Image
 
-data = [5, 0, 4, 4, 4, 0, 0, 4, 5, 3, 4, 4, 5, 2, 0, 1, 2, 3, 3, 5, 2, 2, 1, 2, 4]
+def resize_image(image_path, width, height):
+    with Image.open(image_path) as img:
+        img = img.resize((width, height), Image.ANTIALIAS)
+        return img
 
-# Create a histogram with 6 bins (one for each possible value from 0 to 5)
-plt.hist(data, bins=6, edgecolor='black')
+def create_passport_sheet(images, sheet_size, margin):
+    sheet = Image.new("RGB", sheet_size, (255, 255, 255))
+    x, y = margin
+    for img in images:
+        sheet.paste(img, (x, y))
+        x += img.width + margin[0]
+        if x + img.width + margin[0] > sheet_size[0]:
+            x = margin[0]
+            y += img.height + margin[1]
+    return sheet
 
-# Add axis labels and a title
-plt.xlabel('Graduation Year')
-plt.ylabel('Frequency')
-plt.title('Frequency Distribution of Graduation Years')
+def main(image_paths, passport_size, sheet_size, margin, output_path):
+    passport_images = [resize_image(image_path, *passport_size) for image_path in image_paths]
+    passport_sheet = create_passport_sheet(passport_images, sheet_size, margin)
+    passport_sheet.save(output_path)
 
-# Show the plot
-plt.show()
+if __name__ == "__main__":
+    root = Tk()
+    root.withdraw()
+    filepaths = filedialog.askopenfilenames(title="Select Images",
+                                            filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+    root.destroy()
+    images = [Image.open(filepath) for filepath in filepaths]
+    passport_size = (105, 148)
+    sheet_size = (297, 210)
+    margin = (5, 5)
+    output_path = os.path.join(os.path.dirname(filepaths[0]), "passport_sheet.jpg")
+    main(filepaths, passport_size, sheet_size, margin, output_path)
+
+# Path: 3_Day\Repeat\test.py
