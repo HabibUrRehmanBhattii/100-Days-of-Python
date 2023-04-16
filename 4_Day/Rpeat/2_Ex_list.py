e5cd0646a87b6b1671a42ee5c99d2b4d06d32c46
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2023-04-16 14:15:16 +0200

    repeating

---
+++ 4_Day/Rpeat/2_Ex_list.py
@@ -0,0 +1,34 @@
+import random as r
+
+name_string = input('Give me everybody\'s names, separated by a comma. ')
+names = name_string.split(', ')
+#  Split the string into a list of names
+
+max_names = len(names)
+#  Find the total number of names in the list
+random_selection = r.randint(0, max_names -1)
+print(random_selection)
+
+select_person = names[random_selection]
+
+#  Select a random person from the list of names
+print(f'{select_person} is going to buy the meal today!')
+
+
+
+
+
+
+
+
+
+
+
+#
+# name_list = ['Angela', 'Ben', 'Jenny', 'Michael', 'Chloe']
+#
+# random_selection = r.randint(0, 4)
+#
+# selected_person = name_list[random_selection]
+#
+# print(f'{selected_person} is going to buy the meal today!')
--- 4_Day/Rpeat/list.py
+++ 4_Day/Rpeat/list.py
@@ -8,12 +8,15 @@ print(fruit[-2])
 fruit[0] = 'Pineapple'
 print(fruit)
 
+# append is a method to add the element to the end of the list
 fruit.append('Grape')
 print(fruit)
 
+# insert is a method to add the element to the specific index of the list
 fruit.insert(1, 'Mango')
 print(fruit)
 
+# remove is a method to delete the element from the list
 fruit.remove('Grape')
 print(fruit)
 
@@ -21,4 +24,11 @@ print(fruit)
 # fruit.pop() will delete the last element of the list
 fruit.pop()
 print(fruit)
+
+# extend is a method to add the element to the end of the list
+fruit.extend(['Grape', 'Watermelon'])
+print(fruit)
+
+# count is a method to count the element in the list
+print(len(fruit))
 
