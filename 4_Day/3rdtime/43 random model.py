0a5ba781048140dd479d91b20b84fb6c83725d78
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2024-07-27 14:07:07 -0500

    learing

---
+++ 4_Day/3rdtime/43 random model.py
@@ -0,0 +1,17 @@
+import  random
+
+random_integer = random.randint(1,20)
+
+print(random_integer)
+
+
+random_float = random.random()
+print(random_float)
+
+var = random_float * 5
+
+print(var)
+
+love_score = random.randint(1, 100)
+
+print(f"Your love score is {love_score}")
---
+++ 4_Day/3rdtime/44 day 4.py
@@ -0,0 +1,15 @@
+# Head or Tails
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+
+
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
